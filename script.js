e3368152d99aa1e879b60b7ea223a2e2c9f79a9e
PYSTON7 <patrickpyston@gmail.com>
2026-05-04 16:07:57 +0300

    fix(console):mute

--- script.js
+++ script.js
@@ -11,29 +11,40 @@ document.getElementById("akanForm").addEventListener("submit", function(e) {
         result.textContent = "Please enter your birthdate and select gender.";
         return;
     }
+console.log("gender:", gender);
+console.log("dob:", dob);
 
-   
     const date = new Date(dob);
     const dayNumber = date.getDay(); 
+
+    console.log("Date of Birth:", dob);
+    console.log("Day Number:", dayNumber);
 
     // Day names
     const days = [
         "Sunday", "Monday", "Tuesday",
         "Wednesday", "Thursday", "Friday", "Saturday"
     ];
+    console.log("Days of the week:", days);
 
     // Akan names
     const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    console.log("Akan names:", gender === "male" ? maleNames : femaleNames);
 
-    // Get values
+    // Get date
     const dayName = days[dayNumber];
     const akanName = gender === "male" 
         ? maleNames[dayNumber] 
         : femaleNames[dayNumber];
 
+        console
+
     // Display results
     dayResult.textContent = `You were born on: ${dayName}`;
     result.textContent = `Your Akan name is: ${akanName}`;
+
+    console.log("day born:", dayResult.textContent);
+    console.log("result:", result.textContent);
     
 });
